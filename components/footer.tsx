3c14b0a05dd5025f9b38dade5c3360d0ce548174
qiayue <qiayue@gmail.com>
2024-04-11 18:56:27 +0800

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,33 +7,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
       <div className="flex justify-end">
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://discord.gg/zRxaseCuGq" target="_blank">
-            <SiDiscord size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://twitter.com/miiura" target="_blank">
-            <SiTwitter size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://git.new/morphic" target="_blank">
-            <SiGithub size={18} />
-          </Link>
-        </Button>
+        <a href="https://nibuhuiquwenaima.com/" title="你不会问AI吗？">你不会问AI吗？</a> <a href="https://nibuhuiquwenaima.com/" title="你不会问AI吗？">NiBuHuiWenAIMa.com</a>
       </div>
     </footer>
   )
