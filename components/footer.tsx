0cb591c614ddaac3524703a650f658f0af6bd6f2
qiayue <qiayue@gmail.com>
2024-04-11 19:29:09 +0800

    name

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
       <div className="flex justify-end">
-        <a href="https://nibuhuiquwenaima.com/" title="你不会问AI吗？">你不会问AI吗？</a> <a href="https://nibuhuiquwenaima.com/" title="你不会问AI吗？">NiBuHuiWenAIMa.com</a>
+        <a href="https://nibuhuiquwenaima.com/" title="你不会去问AI吗？">你不会去问AI吗？</a> <a href="https://nibuhuiquwenaima.com/" title="你不会问AI吗？">NiBuHuiWenAIMa.com</a>
       </div>
     </footer>
   )
