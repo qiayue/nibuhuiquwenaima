8178dd11373cb51d63e4b9e30abaada48bd9a9e5
qiayue <qiayue@gmail.com>
2024-04-12 10:47:09 +0800

    logo

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -92,7 +92,6 @@ export function ChatPanel() {
       {/* <IconKuroko className="w-6 h-6 mb-4" /> */}
       <form onSubmit={handleSubmit} className="max-w-2xl w-full px-6">
         <div className="relative flex items-center w-full">
-          <div id="site-logo"></div>
           <Input
             ref={inputRef}
             type="text"
