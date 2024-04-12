2e4d30497c5d6cf681550c81077b9046191a1fa0
qiayue <qiayue@gmail.com>
2024-04-12 10:36:43 +0800

    logo

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,8 @@ import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
-      <div className="flex justify-end">
+    <footer className="w-full p-1 md:p-2 fixed bottom-0 right-0 backdrop-blur">
+      <div className="flex justify-center">
         <a href="https://nibuhuiquwenaima.com/" title="你不会去问AI吗？">你不会去问AI吗？</a> <a href="https://nibuhuiquwenaima.com/" title="你不会问AI吗？">NiBuHuiWenAIMa.com</a>
       </div>
     </footer>
